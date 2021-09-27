46c9c6a5f7ff6db35b8f199a1c374aaa881d63a4
luka <luka.datunashvili01@gmail.com>
2021-09-27 16:14:07 +0400

    Autoload And NameSpaces

---
+++ wp-content/plugins/testfolder/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f797679dbcdebaf06d08c9d2f555e5ba53d28378',
+        'name' => '99559/testfolder',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '99559/testfolder' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f797679dbcdebaf06d08c9d2f555e5ba53d28378',
+            'dev_requirement' => false,
+        ),
+    ),
+);
